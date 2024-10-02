d8e10100035848839a2bcb3c3d7e5398725f2e50
Eslam Alameldin <eashraf36@gmail.com>
2024-10-03 02:07:39 +0300

    .

---
+++ models/MealModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+// 1- Create Schema
+
+const mealSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  chef: {
+    type: String,
+    required: true, // this can be changed later to reference a Chef model
+  },
+});
+
+// 2- Create model
+const mealModel = mongoose.model("Meal", mealSchema);
+
+module.exports = mealModel;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+
+dotenv.config({ path: "config.env" });
+const dbConnection = require("./config/database");
+const mealRoute = require("./routes/mealRoute");
+
+const authRoute = require("./routes/authRoute");
+
+// Connect with db
+dbConnection();
+
+// express app
+const app = express();
+
+// Middlewares
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+  console.log(`mode: ${process.env.NODE_ENV}`);
+}
+
+// Mount Routes
+app.use("/api/v1/meals", mealRoute);
+app.use("/api/v1/auth", authRoute);
+
+// Error Handler
+app.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(500).json({ message: err.message });
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App running running on port ${PORT}`);
+});
